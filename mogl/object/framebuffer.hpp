07fda0e33932bf2608c8e4ab30b6732868d4030b
ninnghazad <ninnghazad@dnophos.de>
2019-03-27 16:43:18 +0100

    Adjust arguments, so it compiles.

--- mogl/object/framebuffer.hpp
+++ mogl/object/framebuffer.hpp
@@ -36,14 +36,17 @@ namespace mogl
         void    setTexture(GLenum attachment, Texture& texture, GLint level = 0);
         void    setDrawBuffer(GLenum buffer);
         void    setDrawBuffers(GLsizei size, const GLenum* buffers);
+        void    invalidate(GLsizei numAttachments, const GLenum* attachments);
+        void    invalidateSubData(GLsizei numAttachments, const GLenum* attachments,
+                                  GLint x, GLint y, GLsizei width, GLsizei height);
         void    set(GLenum property, GLint value);
         bool    isComplete(GLenum target);
-        bool    isValid() const override final;
+        bool    isHandleValid() const override final;
 
     public:
         template <class T>
         void    clear(GLenum buffer, GLint drawbuffer, const T* value);
-        void    clear(GLenum buffer, GLfloat depth, GLint stencil);
+        void    clear(GLenum buffer, GLint drawbuffer, GLfloat depth, GLint stencil);
     };
 }
 
